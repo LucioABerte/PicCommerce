80682610ab149b25c3a088488fe7498d536ecb17
lucio <lucioaberte@gmail.com>
2023-02-07 16:34:16 -0300

    pic ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import React from "react"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+
+const App = () => {    
+    return (
+        <div>
+            <Header />
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+
+import React from "react"
+
+const Cart = () => {
+    return (
+        <main className="cart-page">
+            <h1>Check out</h1>
+        </main>
+    )
+}
+
+export default Cart
