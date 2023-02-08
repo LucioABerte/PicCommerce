9c9882e9c3a7f3a1f22fcc25a5bb51170927e8d9
lucio <lucioaberte@gmail.com>
2023-02-08 17:06:02 -0300

    cart f

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useContext} from "react"
 import { Context } from "../context/Context"
+import PropTypes from "prop-types"
 
 const Image = ({className, img}) => {
 
     const [hovered, setHovered] = useState(false)
-    const {toggleFavorite} = useContext(Context)
+    const {toggleFavorite,addToCart, cartItems, removeFromCart} = useContext(Context)
 
     function heartIcon() {
         if(img.isFavorite) {
@@ -13,8 +14,15 @@ const Image = ({className, img}) => {
             return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
         }
     }
-
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+ 
+    function cartIcon() {
+        const inCart = cartItems.some(item => item.id === img.id)
+        if(inCart) {
+            return <i className="ri-shopping-cart-fill cart" onClick={() => removeFromCart(img.id)}></i>
+        }else if(hovered) {
+            return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+        }
+    }
 
     return (
         <div 
@@ -24,9 +32,18 @@ const Image = ({className, img}) => {
         >
             <img src={img.url} className="image-grid"/>
             {heartIcon()}
-            {cartIcon}
+            {cartIcon()}
         </div>
     )
+}
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
 }
 
 export default Image
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,6 +4,8 @@ const Context = React.createContext()
 const ContextProvider = (props) => {
 
     const [allPics, setAllPics] = useState([])
+    const [cartItems, setCartItems] = useState([])
+
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
     function toggleFavorite(id) {
@@ -16,6 +18,14 @@ const ContextProvider = (props) => {
         setAllPics(updatedArr)
     }
 
+    function addToCart(item) {
+        setCartItems(prevCart => [...prevCart, item])
+    }
+
+    function removeFromCart(id) {
+        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+    }
+
     useEffect(() => {
       
         fetch(url)
@@ -25,7 +35,7 @@ const ContextProvider = (props) => {
     }, [])
     
     return (
-        <Context.Provider value={{allPics, toggleFavorite}}>
+        <Context.Provider value={{allPics, toggleFavorite, addToCart, removeFromCart, cartItems}}>
             {props.children}
         </Context.Provider>
     )
